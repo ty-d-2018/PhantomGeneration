ec4cf40e975683cea5077ec039d8c50a15b4b00d
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-07-27 15:41:10 -0600

    added private functions to SkeletonDataClump
    for the purpose of performing trait method add_op

--- write_functions/src/skeleton.rs
+++ write_functions/src/skeleton.rs
@@ -22,10 +22,26 @@ impl SkeletonDataClump{
     fn change_name(&mut self, new_name: &String){
         self.name = new_name.clone();
     }
+    fn change_modifier(&mut self, new_modifier: &String){
+        self.modifier = Some(new_modifier.clone());
+    }
+    fn add_parameter(&mut self, name: &String, data_type: &String){
+        let param_list: &mut Vec::<(String, String)> = match self.parameters.as_mut(){
+            Some(v) => v,
+            None => {
+                self.parameters = Some(Vec::new());
+                self.parameters.as_mut().unwrap()
+            },
+        };
+
+        param_list.push((name.clone(), data_type.clone()));
+    }
 }
 
 impl EngageCodeOp for SkeletonDataClump{
     fn add_op(&self, tokens: Vec::<String>, first_op: AddCodeOp, second_op: AddCodeOp) -> Self{
+
+        let mut new_skeleton_data: SkeletonDataClump = self.clone();
 
         match first_op{
             AddCodeOp::NAME => {},
