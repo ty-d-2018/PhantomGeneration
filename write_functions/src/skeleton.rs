9f7949f82d36aa2ebc9b9886ce3fcb39cc857ef7
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-07-27 00:10:48 -0600

    implemented new method for SkeletonDataClump

--- write_functions/src/skeleton.rs
+++ write_functions/src/skeleton.rs
@@ -2,9 +2,22 @@ use super::code_op::{ AddCodeOp, EngageCodeOp, Nested};
 
 
 pub struct SkeletonDataClump{
-    modifier: String,
+    modifier: Option::<String>,
     name: String,
-    parameters: Vec::<(String, String)>,
-    return_type: String,
+    parameters: Option::<Vec::<(String, String)>>,
+    return_type: Option::<String>,
+    code_block: Option::<Vec::<String>>,
+}
+
+impl SkeletonDataClump{
+    pub fn new(name: &String,) -> SkeletonDataClump{
+        SkeletonDataClump{
+            modifier: None,
+            name: name.clone(),
+            parameters: None,
+            return_type: None,
+            code_block: None,
+        }
+    }
 }
 
