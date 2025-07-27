23ab1513324637b7e4ac943726c3fdfbff331183
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-07-27 00:07:18 -0600

    created nested enum

--- write_functions/src/code_op.rs
+++ write_functions/src/code_op.rs
@@ -9,7 +9,13 @@ pub enum AddCodeOp{
     LIFETIME,
 }
 
+pub enum Nested{
+    Empty,
+    Parameter,
+    Block,
+}
+
 pub trait EngageCodeOp{
-    fn add_op(statement: Self, tokens: String, op: AddCodeOp) -> Self;
+    fn add_op(statement: Self, tokens: String, op: AddCodeOp, nested: Nested) -> Self;
     fn create_op(tokens: String, op: AddCodeOp) -> Self;
 }
