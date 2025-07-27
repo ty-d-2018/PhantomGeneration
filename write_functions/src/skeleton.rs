c56687a617d506dd2d0dcd454c6ecb643ec977be
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-07-27 00:04:59 -0600

    created skeleton data clump for structs

--- write_functions/src/skeleton.rs
+++ write_functions/src/skeleton.rs
@@ -1,48 +1,10 @@
-use std::iter::Iterator;
+use super::code_op::{ AddCodeOp, EngageCodeOp };
 
-pub struct SK_Function{
-    modifier: Modifier,
-    pub name: String,
-    parameters: Option::<Vec::<(String, Box::<dyn PType>)>>,
-}
 
-enum Modifier{
-    Private,
-    Public,
-}
-
-pub trait PType{
-    fn get_name(&self) -> String;
-}
-
-impl<'a> SK_Function{
-    pub fn get_p_iterator() -> ParamIterator<'a>{
-        todo!();
-    }
-}
-
-pub struct ParamIterator<'a>{
-    index: usize,
-    parameters: &'a Vec::<(String, Box::<dyn PType>)>,
-}
-
-impl<'a> ParamIterator<'a>{
-    pub fn new(parameters: &'a Vec::<(String, Box::<dyn PType>)>) -> Self{
-        ParamIterator{
-            index: 0,
-            parameters,
-        }
-    }
-}
-
-impl<'a> Iterator for ParamIterator<'a>{
-    type Item = &'a (String, Box::<dyn PType>);
-
-    fn next(&mut self) -> Option::<Self::Item>{
-        let mut previous_count = self.index;
-        self.index += 1;
-
-        self.parameters.get(previous_count)
-    }
+pub struct SkeletonDataClump{
+    modifier: String,
+    name: String,
+    parameters: Vec::<(String, String)>,
+    return_type: String,
 }
 
