d0c4758f860d8e6be9cc8b84d1402806df84c414
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-08-04 03:09:57 -0600

    moved markalphabet into sub module

--- write_functions/src/stance.rs
+++ write_functions/src/stance.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-use std::iter::Iterator;
 
 pub fn create_label(name: &String) -> String{
     name.clone()
@@ -21,58 +19,65 @@ pub enum Marks{
     Operator,
 }
 
-pub struct MarkAlphabet{
-    order: HashMap::<u32, Marks>,
-    count: u32,
-}
-
-impl MarkAlphabet{
-    pub fn new() -> MarkAlphabet{
-        let max: u32 = 5;
-        MarkAlphabet{
-            order: MarkAlphabet::get_order(max),
-            count: 0,
-        }
-    }
-
-    fn get_order(max: u32) -> HashMap::<u32, Marks>{
-        let mut map: HashMap = HashMap::new();
-        for i in 0..max{
-            match i{
-                0 => map.insert(i, Marks::DataType),
-                1 => map.insert(i, Marks::Marker),
-                2 => map.insert(i, Marks::Modifier),
-                3 => map.insert(i, Marks::Name,),
-                4 => map.insert(i, Marks::Operator),
-            };
-        }
-
-        map
-    }
-
-    fn get_from_map(&self, index: u32) -> Result::<&Marks, u32>{
-        self.order.get(&index).ok_or(self.max)
-    }
-
-}
-
-impl Iterator for MarkAlphabet{
-    type Item = &Marks;
-
-    fn next(&mut self) -> Option::<Self::Item>{
-        let mark_alpha: Option::<&Marks> = self.get_from_map(self.count).ok();
-        if let Option::None = mark_alpha{
-            self.count = 0;
-        }else{
-            self.count += 1;
-        }
-
-        mark_alpha
-    }
-}
 
 impl Marks{
-    pub fn get_alphabet_iter() -> MarkAlphabet{
-        MarkAlphabet::new()
+    pub fn get_alphabet_iter() -> MarkIterator::MarkAlphabet{
+        MarkIterator::MarkAlphabet::new()
+    }
+}
+
+pub mod MarkIterator{
+
+    use std::collections::HashMap;
+    use std::iter::Iterator;
+
+    pub struct MarkAlphabet{
+        order: HashMap::<u32, Marks>,
+        count: u32,
+    }
+
+    impl MarkAlphabet{
+        pub fn new() -> MarkAlphabet{
+            let max: u32 = 5;
+            MarkAlphabet{
+                order: MarkAlphabet::get_order(max),
+                count: 0,
+            }
+        }
+
+        fn get_order(max: u32) -> HashMap::<u32, Marks>{
+            let mut map: HashMap = HashMap::new();
+            for i in 0..max{
+                match i{
+                    0 => map.insert(i, Marks::DataType),
+                    1 => map.insert(i, Marks::Marker),
+                    2 => map.insert(i, Marks::Modifier),
+                    3 => map.insert(i, Marks::Name,),
+                    4 => map.insert(i, Marks::Operator),
+                };
+            }
+
+            map
+        }
+
+        fn get_from_map(&self, index: u32) -> Result::<&Marks, u32>{
+            self.order.get(&index).ok_or(self.max)
+        }
+
+    }
+
+    impl Iterator for MarkAlphabet{
+        type Item = &Marks;
+
+        fn next(&mut self) -> Option::<Self::Item>{
+            let mark_alpha: Option::<&Marks> = self.get_from_map(self.count).ok();
+            if let Option::None = mark_alpha{
+                self.count = 0;
+            }else{
+                self.count += 1;
+            }
+
+            mark_alpha
+        }
     }
 }
