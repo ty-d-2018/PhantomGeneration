bf73d56dc82a96e42af45985fa83468ff053de1e
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-08-12 17:59:22 -0600

    create trait Calculate_L_R

--- represent/src/process.rs
+++ represent/src/process.rs
@@ -1,2 +1,16 @@
 
-pub trait Calculate{}
+pub trait Calculate_L_R{
+    type Item;
+    type Key;
+    type Value;
+    //Can combine itself into a new Data
+    type Data;
+    type Output;
+
+    fn set_element(el: &Self::Item) -> Self::Value;
+    fn element_to_data(operator: &mut Self, key: &Self::Key, element: &Self::Value) -> Self::Data;
+    fn size(&self) -> usize;
+    fn set_data(operator: &mut Self, data: Self::Data) -> Result::<(), ()>;
+    fn calculate(&mut self);
+    fn get_output(&self) -> Option::<Self::Output>;
+}
